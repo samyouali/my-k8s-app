909fd1d5ee3ff0f8cf090d030459a6894a944df9
samyouali <2187632@cegepmontpetit.ca>
2025-04-30 11:08:25 -0400

    commit salutworld

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Salut\n');
+    res.end('Salut World\n');
 });
 
 
